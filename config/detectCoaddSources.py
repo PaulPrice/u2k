fb232529ab95778211802ab38d19d67a573fee27
Paul Price <price@astro.princeton.edu>
2026-06-17 15:31:03 -0400

    disable fancy adaptive detection
    It produces VERY large blends on some of the NIR data.

--- config/detectCoaddSources.py
+++ config/detectCoaddSources.py
@@ -4,3 +4,8 @@ config.detection.tempWideBackground.useApprox = False
 config.detection.reEstimateBackground = True
 config.detection.background.binSize = 128
 config.detection.background.useApprox = False
+
+# Disable fancy new adaptive detection
+config.detection.doThresholdScaling = False
+config.detection.doBackgroundTweak = False
+config.detection.doBrightPrelimDetection = False
