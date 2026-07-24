e76bfe7ba888df50ca923ff7616b4988048a56be
Paul Price <price@astro.princeton.edu>
2026-07-24 12:27:04 -0400

    measure: ensure exposures have WCS set
    for both the exposure and the aperture corrections.
    I don't think this is necessary since the fixes to
    the warping, but I don't think it does any harm.

--- python/u2k/measure.py
+++ python/u2k/measure.py
@@ -1,5 +1,41 @@
 from lsst.pipe.tasks.multiBand import MeasureMergedCoaddSourcesTask
 from lsst.drp.tasks.forcedPhotCoadd import ForcedPhotCoaddTask
+from lsst.meas.algorithms import CoaddBoundedField
+
+
+def checkExposure(exposure, wcs, plugins):
+    """Check that the exposure is valid for measurement
+
+    Fix it if not.
+
+    Parameters
+    ----------
+    exposure : `lsst.afw.image.Exposure`
+        Exposure to check.
+    wcs : `lsst.afw.geom.Wcs`
+        World coordinate system to use.
+    plugins : `lsst.meas.base.Plugins`
+        Measurement plugins being used.
+    """
+    # Ensure we have the required mask planes
+    maskPlanes = set()
+    maskPlanes |= set(plugins["base_PixelFlags"].masksFpCenter)
+    maskPlanes |= set(plugins["base_PixelFlags"].masksFpAnywhere)
+    maskPlanes |= set(plugins["base_PsfFlux"].badMaskPlanes)
+    for name in maskPlanes:
+        exposure.mask.addMaskPlane(name)
+
+    # Ensure we have a WCS
+    if exposure.getWcs() is None:
+        exposure.setWcs(wcs)
+
+    # Ensure we have a coadd apcorr map with the correct WCS
+    acm = exposure.getInfo().getApCorrMap()
+    for key in acm:
+        field = acm[key]
+        if not isinstance(field, CoaddBoundedField) or field.getCoaddWcs() is not None:
+            continue
+        acm[key] = CoaddBoundedField(field.getBBox(), wcs, field.getElements(), field.getDefault())
 
 
 class MeasureTask(MeasureMergedCoaddSourcesTask):
@@ -9,14 +45,7 @@ class MeasureTask(MeasureMergedCoaddSourcesTask):
     """
 
     def run(self, exposure, sources, skyInfo, *args, **kwargs):
-        maskPlanes = set()
-        maskPlanes |= set(self.config.measurement.plugins["base_PixelFlags"].masksFpCenter)
-        maskPlanes |= set(self.config.measurement.plugins["base_PixelFlags"].masksFpAnywhere)
-        maskPlanes |= set(self.config.measurement.plugins["base_PsfFlux"].badMaskPlanes)
-        for name in maskPlanes:
-            exposure.mask.addMaskPlane(name)
-
-        exposure.setWcs(skyInfo.wcs)
+        checkExposure(exposure, skyInfo.wcs, self.config.measurement.plugins)
         return super().run(exposure, sources, skyInfo, *args, **kwargs)
 
 
@@ -27,12 +56,5 @@ class ForcedTask(ForcedPhotCoaddTask):
     """
 
     def run(self, measCat, exposure, refCat, refWcs, *args, **kwargs):
-        maskPlanes = set()
-        maskPlanes |= set(self.config.measurement.plugins["base_PixelFlags"].masksFpCenter)
-        maskPlanes |= set(self.config.measurement.plugins["base_PixelFlags"].masksFpAnywhere)
-        maskPlanes |= set(self.config.measurement.plugins["base_PsfFlux"].badMaskPlanes)
-        for name in maskPlanes:
-            exposure.mask.addMaskPlane(name)
-
-        exposure.setWcs(refWcs)
+        checkExposure(exposure, refWcs, self.config.measurement.plugins)
         return super().run(measCat, exposure, refCat, refWcs, *args, **kwargs)
