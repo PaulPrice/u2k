asiaa: fix variance calculation
Since the coadd is an average rather than a sum, the variance
goes as the inverse of the exposure time.

python/u2k/asiaa.py
@@ -62,9 +62,10 @@ def calibrateAsiaa(
         Configuration for the calibration.
     """
     exposure.mask.array[:] = 0
-    # The variance goes as the background, which goes as the exposure time
-    # Somehow, some of the times can be negative...
-    exposure.variance.array[:] = np.clip(timeImage.array, 0.0, None)
+    # The variance goes as the inverse of the exposure time.
+    variance = 1.0 / np.clip(timeImage.array, 0.0, None)
+    variance[~np.isfinite(variance)] = 0.0
+    exposure.variance.array[:] = variance
 
     good = np.isfinite(exposure.image.array)
     exposure.image.array[~good] = 0.0